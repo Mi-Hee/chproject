564fc6b1ad95e3ad52e0038898f1a7eb943c4b52
Mi-Hee <eeh716@naver.com>
2024-01-15 17:18:24 +0900

    2024.01.15

--- src/main/java/org/choongang/board/entities/BoardData.java
+++ src/main/java/org/choongang/board/entities/BoardData.java
@@ -1,17 +1,58 @@
 package org.choongang.board.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
+import org.choongang.commons.entities.Base;
 import org.choongang.commons.entities.BaseMember;
+import org.choongang.member.entities.Member;
+
+import java.util.UUID;
 
 @Data
 @Entity
-public class BoardData extends BaseMember {
+public class BoardData extends Base {
     @Id @GeneratedValue
     private Long seq;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "boardSeq")
+    private Board board;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "memberSeq")
+    private Member member;
+
+    private String gid = UUID.randomUUID().toString();
+
+    private String poster; // 작성자
+    private String guestPw; // 비회원 비밀번호
+
+    private boolean notice; // 공지글 여부 - true : 공지글
 
     private String subject;
     private String content;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
